3e1309144b4f027ebacb4117bdd28467b98ea2f9
firas <firas.mosbehi@insat.ucar.tn>
2023-06-03 13:30:51 +0100

    fixed ui

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,41 +3,16 @@ import { Link } from "react-router-dom";
 import "./item.css";
 
 export default function Item({ id, name, description, price, photo }) {
-  const [base64Image, setBase64Image] = React.useState("");
-
-  React.useEffect(() => {
-    const convertToBase64 = async () => {
-      try {
-        console.log(0);
-        const response = await fetch(`file://${photo}`);
-        console.log(1);
-        const blob = await response.blob();
-        const reader = new FileReader();
-
-        reader.onloadend = () => {
-          const base64String = reader.result;
-          setBase64Image(base64String);
-        };
-
-        reader.readAsDataURL(blob);
-      } catch (error) {
-        console.error("Error converting image to Base64:", error);
-      }
-    };
-
-    convertToBase64();
-  }, [photo]);
-
   return (
     <div className="card">
       <div
         className="card-image"
-        style={{ backgroundImage: `url(${base64Image})` }}
+        style={{ backgroundImage: `url(data:image/png;base64,${photo})` }}
       ></div>
       <h3 className="card-title">{name}</h3>
       <p className="card-price">${price}</p>
       <Link
-        to={`/details?id=${id}&name=${name}&description=${description}&price=${price}&photo=${photo}`}
+        to={`/details?id=${id}&name=${name}&description=${description}&price=${price}&photo=data:image/png;base64,${photo}`}
       >
         <button className="card-button">View Details</button>{" "}
       </Link>
